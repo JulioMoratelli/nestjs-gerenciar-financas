3793d1a09c6b0448801aa584a7ca080f94d767fb
Fabio Silva <fabio_fatec@hotmail.com>
2023-07-14 16:41:19 -0300

    comments

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      stopAtFirstError: true,
       transform: true,
     }),
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   await app.listen(3000);
+  console.log(`App is running on: ${await app.getUrl()}`);
 }
 bootstrap();
