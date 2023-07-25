afa0e0914583969ddfcf53ef851397a2bbc40d9f
JulioMoratelli <l3xpooh@gmail.com>
2023-07-25 00:14:02 -0300

    testes

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+    root: './',
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+});
